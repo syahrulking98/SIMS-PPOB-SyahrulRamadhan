9cba7a4f9502921ffd86e8da4184bea069d47cf9
syahrul ramadhan <syahrulramadhan.real@gmail.com>
2025-07-15 05:07:22 +0700

    chore: show errors in production for debugging

--- app/Config/Boot/production.php
+++ app/Config/Boot/production.php
@@ -9,10 +9,10 @@
  |
  | If you set 'display_errors' to '1', CI4's detailed error report will show.
  */
-error_reporting(E_ALL & ~E_DEPRECATED);
+error_reporting(E_ALL); // <-- pastikan semua error ditampilkan
 // If you want to suppress more types of errors.
 // error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-ini_set('display_errors', '0');
+ini_set('display_errors', '1'); // <-- aktifkan error
 
 /*
  |--------------------------------------------------------------------------
@@ -22,4 +22,4 @@ ini_set('display_errors', '0');
  | the system. It's not widely used currently, and may not survive
  | release of the framework.
  */
-defined('CI_DEBUG') || define('CI_DEBUG', false);
+defined('CI_DEBUG') || define('CI_DEBUG', true); // <-- nyalakan debug mode
